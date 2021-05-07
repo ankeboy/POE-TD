907e06833f82a7e3dd95e7e974036b6efa3b3d0f
Dimora <63793884+ankeboy@users.noreply.github.com>
2021-05-07 19:17:22 +0200

    Added "How to play"
    Added annotated screenshots of How to Play. Screenshots are images converted and saved into a Sprits[] array. SliderPanel and ImagePanel are added. ImagePanel.image changes based on the order of the image in the array whenever the buttons are pressed.
    
    PanelOpener.cs contains the instruction to slide the panel into view and out of view.
    
    TutorialPages.cs contains the information of the images and mechanism of changing these on button press.

---
+++ Assets/TutorialPages.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TutorialPages : MonoBehaviour
+{
+    public Sprite[] pages;
+    public Button nextButton;
+    public Button prevButton;
+    public GameObject imagePanel;
+
+
+    int counter = 1;
+
+    public void NextPage()
+    {
+        counter += 1;
+        imagePanel.GetComponent<Image>().sprite = pages[counter - 1];
+        prevButton.interactable = true;
+        if (counter == pages.Length)
+        {
+            nextButton.interactable = false;
+        }
+    }
+
+    public void PrevPage()
+    {
+        counter -= 1;
+        imagePanel.GetComponent<Image>().sprite = pages[counter - 1];
+        nextButton.interactable = true;
+        if (counter == 1)
+        {
+            prevButton.interactable = false;
+        }
+    }
+}
